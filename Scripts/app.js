76e1929d70946422e3e9e53e3d0e81f6ff9201c3
Gamaliel <chamba123g@gmail.com>
2025-01-24 09:43:42 -0600

    Fix the problem with front end using cors reading videogames perfectly -Gamaliel Romualdo

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,26 +6,33 @@ async function fetchGames() {
     try {
         const response = await fetch(' http://localhost:300/api/videogames');
         if (!response.ok) throw new Error('Failed to fetch games');
-        
         const games = await response.json();
+        console.log('Games recived:', games)
         displayGames(games);
     } catch (error) {
         console.error('Error fetching games:', error);
     }
 }
-
+//Tony fixee el problema ahora deberia de estar funcionando pero si podes hacer que esto encuentre con el js los generos por el videojuego fuera increible
+//tecnicamente lee todos los datos si seguis esta logica de todo el codigo podras entender como acceder a todas las apis.
+//te lo dejo para que lo testees todo lo que querras 
 function displayGames(games) {
-    const container = document.getElementById('games-container');
-    container.innerHTML = '';
-
     games.forEach(game => {
-        const gameCard = document.createElement('div');
-        gameCard.className = 'game-card';
-        gameCard.innerHTML = `
-            <h3>${videogame.gameName}</h3>
-            <p>${videogame.Description}</p>
-            <img src="${videogame.Image}" alt="${game.gameName}">
-        `;
-        container.appendChild(gameCard);
+        console.log('Game:', game);
+        const name = game.gameName;
+        const description = game.Description;
+        const image = game.image || `default-image.jpg`
+
+        console.log(`Gamename ${name}, descriptiopn ${description}, img ${image}`);
+
+        const gameContainer = document.createElement(`div`);
+        gameContainer.classList.add('game=item');
+        gameContainer.innerHTML = `
+        <h3>${name || 'No Name'}</h3>
+        <p>${description || 'No Description Available'}</p>
+        <img src="${image}" alt="${name || 'No Image'}">
+    `;
+    document.body.appendChild(gameContainer);
     });
-}
+    
+};
